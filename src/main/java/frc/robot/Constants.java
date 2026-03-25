8dec82cc1f3b519538750ac5702138b97d73e3f3
CIA Robotics - FRC 10317 <jaron.mcmurry@chaffeer2.org>
2026-03-25 18:51:40 -0500

    wheel base direction switch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -74,7 +74,7 @@ public final class Constants {
 
     // This value is multiplied by the joystick value when rotating the robot to
     // help avoid turning too fast and beign difficult to control
-    public static final double DRIVE_SCALING = 0.7;
+    public static final double DRIVE_SCALING = -0.7;
     public static final double ROTATION_SCALING = 0.8;
   } 
 }
